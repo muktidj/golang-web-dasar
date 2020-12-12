749ac135d4d0239efee464c8a3c52c3a062e06fe
muktidj <muktidwijatmoko@gmail.com>
2020-12-12 14:00:51 +0700

    Template Render HTML and CSS

--- main.go
+++ main.go
@@ -2,23 +2,47 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	pathpkg "path"
 )
 
+
+
 func main() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		var filepath = pathpkg.Join("views", "index.html")
+		var tmpl, err = template.ParseFiles(filepath)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var data = map[string]interface{}{
+			"title" : "Learning Golang",
+			"name"	: "Ghost Man",
+		}
+
+		err = tmpl.Execute(writer, data)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+		}
+
+	})
+
+
 	http.Handle("/static/",
 		http.StripPrefix("/static/",
 			http.FileServer(http.Dir("assets"))))
 
-	fmt.Println("server started at localhost:9000")
-	http.ListenAndServe(":9000", nil)
+	address := "localhost:9000"
+	fmt.Println("Server started at %s\n", address)
+	//Cara Pertama untuk Start Server
 
-	/* Cara Kedua
-	server := new(http.Server)
-	server.Addr = address
-	err := server.ListenAndServe()
+	err := http.ListenAndServe(address, nil)
 	if err != nil {
-	    fmt.Println(err.Error())
+		fmt.Println(err.Error())
 	}
-	*/
+
+
 }
