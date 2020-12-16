b040e57458646760d64262163e4d345f161387ea
muktidj <muktidwijatmoko@gmail.com>
2020-12-16 22:41:49 +0700

    Template: Actions & Variabel

--- main.go
+++ main.go
@@ -4,45 +4,43 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	pathpkg "path"
 )
 
+type M map[string]interface{}
 
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		var filepath = pathpkg.Join("views", "index.html")
-		var tmpl, err = template.ParseFiles(filepath)
-		if err != nil {
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
+		var data = M{"name": "Batman"}
+		var tmpl = template.Must(template.ParseFiles(
+			"views/index.html",
+			"views/_header.html",
+			"views/_message.html",
+		))
 
-		var data = map[string]interface{}{
-			"title" : "Learning Golang",
-			"name"	: "Ghost Man",
-		}
-
-		err = tmpl.Execute(writer, data)
+		var err = tmpl.ExecuteTemplate(w, "index", data)
 		if err != nil {
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		var data = M{"name": "Batman"}
+		var tmpl = template.Must(template.ParseFiles(
+			"views/about.html",
+			"views/_header.html",
+			"views/_message.html",
+		))
+
+		var err = tmpl.ExecuteTemplate(w, "about", data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 
 	})
 
 
-	http.Handle("/static/",
-		http.StripPrefix("/static/",
-			http.FileServer(http.Dir("assets"))))
 
-	address := "localhost:9000"
-	fmt.Println("Server started at %s\n", address)
-	//Cara Pertama untuk Start Server
-
-	err := http.ListenAndServe(address, nil)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-
+	fmt.Println("server started at localhost:9000")
+	http.ListenAndServe(":9000", nil)
 }
