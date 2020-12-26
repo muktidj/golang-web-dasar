f37a276ce3909d9cb3100f66b6410d66f1fa7b56
muktidj <muktidwijatmoko@gmail.com>
2020-12-26 14:35:49 +0700

    Form Value

--- main.go
+++ main.go
@@ -2,25 +2,51 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		switch request.Method {
-			case "POST":
-				writer.Write([]byte("post"))
-
-			case "GET":
-				writer.Write([]byte("get"))
-
-			default:
-				http.Error(writer, "", http.StatusBadRequest )
-
-
-		}
-	})
+	http.HandleFunc("/", routeIndexGet)
+	http.HandleFunc("/process", routeSubmitPost)
 
 	fmt.Println("server started at localhost:9000")
 	http.ListenAndServe(":9000", nil)
+}
+
+func routeIndexGet(write http.ResponseWriter, read *http.Request){
+	if read.Method == "GET" {
+		var tmpl = template.Must(template.New("form").ParseFiles("view.html"))
+		var err = tmpl.Execute(write, nil)
+
+		if err != nil {
+			http.Error(write, err.Error(), http.StatusInternalServerError)
+		}
+
+		return
+	}
+
+	http.Error(write, "", http.StatusBadRequest)
+}
+
+func routeSubmitPost(write http.ResponseWriter, read *http.Request)  {
+	if read.Method == "POST" {
+		var tmpl = template.Must(template.New("result").ParseFiles("view.html"))
+
+		if err := read.ParseForm(); err != nil {
+			http.Error(write, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var name = read.FormValue("name")
+		var message = read.FormValue("message")
+
+		var data = map[string]string{"name": name, "message": message}
+
+		if err := tmpl.Execute(write, data); err != nil {
+			http.Error(write, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(write, "", http.StatusBadRequest)
 }
