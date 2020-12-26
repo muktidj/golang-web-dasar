c75cafb5a5fc059f8ef851c91245a101845b0436
muktidj <muktidwijatmoko@gmail.com>
2020-12-26 15:18:07 +0700

    Form Upload File

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
+	"io"
+	"path/filepath"
 )
 
 func main() {
@@ -15,38 +18,66 @@ func main() {
 }
 
 func routeIndexGet(write http.ResponseWriter, read *http.Request){
-	if read.Method == "GET" {
-		var tmpl = template.Must(template.New("form").ParseFiles("view.html"))
+	if read.Method != "GET" {
+		http.Error(write, "", http.StatusBadRequest)
+		return
+		}
+
+		var tmpl = template.Must(template.ParseFiles("view.html"))
 		var err = tmpl.Execute(write, nil)
 
 		if err != nil {
+
 			http.Error(write, err.Error(), http.StatusInternalServerError)
-		}
 
-		return
 	}
-
-	http.Error(write, "", http.StatusBadRequest)
 }
 
 func routeSubmitPost(write http.ResponseWriter, read *http.Request)  {
-	if read.Method == "POST" {
-		var tmpl = template.Must(template.New("result").ParseFiles("view.html"))
-
-		if err := read.ParseForm(); err != nil {
-			http.Error(write, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		var name = read.FormValue("name")
-		var message = read.FormValue("message")
-
-		var data = map[string]string{"name": name, "message": message}
-
-		if err := tmpl.Execute(write, data); err != nil {
-			http.Error(write, err.Error(), http.StatusInternalServerError)
-		}
+	if read.Method != "POST" {
+		http.Error(write, "", http.StatusBadRequest)
 		return
 	}
-	http.Error(write, "", http.StatusBadRequest)
+
+	if err := read.ParseMultipartForm(1024); err != nil {
+		http.Error(write, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	alias := read.FormValue("alias")
+
+	uploadedFile, handler, err := read.FormFile("file")
+	if err != nil {
+		http.Error(write, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer uploadedFile.Close()
+
+
+	dir, err := os.Getwd()
+	if err != nil {
+		http.Error(write, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	filename := handler.Filename
+	if alias != "" {
+		filename = fmt.Sprintf("%s%s", alias, filepath.Ext(handler.Filename))
+	}
+
+	fileLocation := filepath.Join(dir, "files", filename)
+	targetFile, err := os.OpenFile(fileLocation, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		http.Error(write, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer targetFile.Close()
+
+	if _, err := io.Copy(targetFile, uploadedFile); err != nil {
+		http.Error(write, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	write.Write([]byte("done"))
+
 }
